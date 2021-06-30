c4faac36101cfad0daf5cf3f180fcd1cbb1d089b
Colin Tessarzick <colin@visionranger.com>
2021-06-30 14:41:24 +0200

    Initial Commit

---
+++ Mondo/Mondo.h
@@ -0,0 +1,18 @@
+//
+//  Mondo.h
+//  Mondo
+//
+//  Created by Colin Tessarzick on 30.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mondo.
+FOUNDATION_EXPORT double MondoVersionNumber;
+
+//! Project version string for Mondo.
+FOUNDATION_EXPORT const unsigned char MondoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mondo/PublicHeader.h>
+
+
