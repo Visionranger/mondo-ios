8c8ee9ecbdd26a4e153113062cfeaf14ed4259b5
Colin Tessarzick <colin@visionranger.com>
2021-06-30 15:14:42 +0200

    -

---
+++ Sources/MondoPackage/ScreenProperties.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenProperties.swift
+//  Mondo
+//
+//  Created by Colin Tessarzick on 30.06.21.
+//
+
+import UIKit
+
+extension CGFloat {
+    
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
