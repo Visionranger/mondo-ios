35b4fc1841713a499564a1fbf80fe3add548b98d
Colin Tessarzick <colin@visionranger.com>
2021-07-02 14:48:13 +0200

    fix: initializer for internal use

--- Sources/MondoPackage/FormValidation.swift
+++ Sources/MondoPackage/FormValidation.swift
@@ -12,11 +12,13 @@ public class FormValidation {
     
     public let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
     
+    
+    public init() { }
+    
+    
     public func isValidEmail(_ email: String) -> Bool {
-        
-        let emailRegEx = emailRegEx
 
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", self.emailRegEx)
         
         return emailPred.evaluate(with: email)
     }
